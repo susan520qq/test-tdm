6c73f84f4884b30b11e3ee62b5e9cdd5323d4ef5
susan520qq <103975516+susan520qq@users.noreply.github.com>
2022-05-12 17:59:33 +0800

    upload btn, create btn

--- src/confirm/btn.tsx
+++ src/confirm/btn.tsx
@@ -1,3 +1,4 @@
+import React,{useState, useEffect} from 'react';
 import { Breadcrumbs} from '@material-ui/core';
 import{ FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import{
@@ -5,6 +6,34 @@ faDownload,
 faPen
 } from '@fortawesome/free-solid-svg-icons';
 import{ Dropdown} from 'primereact/dropdown';
+import Icon from 'styles/img/icon.svg';
+import Confirm from 'common/Confirm';
+
+useEffect(() => {
+  setScheduleList(props.scheduleList.map((file) => ({label:file.name, value: file.key})));
+  setDefaultSchedule(props.defaultSchedule);
+  setSelectSchedule(props.selectSchedule);
+},[props.scheduleList,props.defaultFile]);
+
+const onSave =(defaultSchedule) => {
+  if(defaultSchedule){
+    props.onSave && props.onSave(defaultSchedule);
+  }
+};
+
+const onDelete =() =>{
+  setIsConfirm(false);
+  props.onDelete && props.onDelete(selectSchedule);
+};
+
+const onCreate =() =>{
+  props.onCreate && props.onCreate();
+};
+
+const onUpload =(event) =>{
+  props.onUpload && props.onUpload(event.target.files[0]);
+};
+
 
 <Breadcrumbs>
   {[fileName, detailKeyStage].map((name) =>{
@@ -39,6 +68,23 @@ import{ Dropdown} from 'primereact/dropdown';
   
   <button >
     <svg className="btn-icon>
-                    <use xlinkHref={Icon + '#setting'}></use>
-                    </svg>
+         <use xlinkHref={Icon + '#setting'}></use>
+     </svg>
+  </button>
+                    
+  <button className="info" onClick={onCreate}>
+                      <svg className="btn-icon">
+                        <use xlinkHref={Icon + '#add'}></use>
+                        </svg>
+                      <span>Add</span>
                     </button>
+                    
+                    
+   <input className="noshow" type='file' id='f-upload' onChange={onUpload} value='' accept='application/JSON' />
+     <button className="info fontawesome">
+                      <label htmlFor='f-upload'>
+                      <FontAwesomeIcon icon={faUpload}/>
+                      <span>upload</span>
+                      </label>
+                    </button>                
+                    
